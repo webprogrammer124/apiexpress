5a378def6066f976a7cffc5be1c43229f4580774
Your Name <you@example.com>
2021-01-06 11:16:10 +0500

    api

---
+++ Users.js
@@ -0,0 +1,7 @@
+
+let users=[
+    {name:'seema',id:1,email:'seemagreat.911@gmail.com',password:'222'},
+    {name:'bisma',id:2,email:'bismatouseeq2020@gmail.com',password:'222'},
+    
+]
+module.exports = users
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path')
+const bodyParser= require('body-parser');
+const users= require('./Users')
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.listen(PORT,(req,res)=>{
+    console.log('server is running at port:',PORT)
+})
+console.log(users)
+app.get('/api/users',(req,res)=>{
+    res.json(users)
+})
+app.get('api/users/:id',(req,res)=>{
+   let id =req.params.id
+   console.log(id)
+   let result =users.filter((item)=>item.id ==id)
+    res.json(result[0])
+})
